b6000640a42aa6516fb215e3434b89450b8211a5
Khenziii <khenzii.was.taken@gmail.com>
2024-07-30 20:05:17 +0200

    feat: define the constants

--- guess-the-number.s
+++ guess-the-number.s
@@ -1,19 +1,22 @@
-.section .data
-msg:    .asciz "Hello, World!\n"    # The message to print
+# In this section, constants are defined.
+# `rodata` stands for `read-only data`.
+.section .rodata
+startup_message: .asciz "I have generated a number in range 0-100, can you figure out which one is it?"
+prompt: .asciz "Your guess: "
+number_smaller_message: .asciz "Generated number is smaller than your guess!"
+number_larger_message: .asciz "Generated number is larger than your guess!"
+final_message: .asciz "You got the number right! It took you %d tries."
+final_message_format: .asciz "%d"
+
+# This section contains uninitialized variables.
+.section .bss
+# Our random number is in range 0-100, meaning that it fits into one byte.
+# One byte consists of 8 bits, which is enough to store an non-negative int with value up to 255.
+.lcomm random_number, 1
 
 .section .text
 .global _start
 
 _start:
-    # write(int fd, const void *buf, size_t count)
-    mov     $1, %rax               # syscall number for sys_write (1)
-    mov     $1, %rdi               # file descriptor 1 is stdout
-    mov     $msg, %rsi             # pointer to the message
-    mov     $14, %rdx              # message length (14 bytes)
-    syscall                        # make the syscall
-
-    # exit(int status)
-    mov     $60, %rax              # syscall number for sys_exit (60)
-    xor     %rdi, %rdi             # exit status 0
-    syscall                        # make the syscall
+    # ...
 
